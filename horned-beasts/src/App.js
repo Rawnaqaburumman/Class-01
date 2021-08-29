c3448dbd5504d2398fc5a3f16f846de52d27c889
Rawnaqaburumman <engrawnaqaburummn@gmail.com>
2021-08-29 23:24:46 +0300

    k

---
+++ horned-beasts/src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import Main from './component/Main';
+
+class App extends React.Component{
+    render(){
+    return (
+        <div className = 'App'>
+
+<Main/>
+
+
+
+</div>
+    )
+}}
+
+export default App;
+
+
